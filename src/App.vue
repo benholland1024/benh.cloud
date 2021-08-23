a09a74309adc49417130418e31d6e278c04ac3c6
Ben Holland <bhollan5@kent.edu>
2021-08-23 17:59:23 -0400

    Adding dark mode, saved to localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,57 @@
 <template>
+  <div :class="{ 'dark-mode': dark_mode }">
   
-  <header>
-    <h1>&#9639; &nbsp; Ben H. &nbsp; &#9640;</h1>
-    <p id="page-subtitle">Hi! This is my personal website.</p>
-  </header>
+    <div id="tooltip" v-if="0">Hi!</div>
 
-  <nav id="nav-links">
-    <router-link class="nav-link blue" to="/">Bio</router-link>
-    <router-link class="nav-link blue2" to="/portfolio">Portfolio</router-link>
-    <router-link class="nav-link brown" to="/plans">Plans</router-link>
-    <router-link class="nav-link orange" to="/dash">Dash</router-link>
-  </nav>
+    <img src="./assets/misc/moon.png" id="dark-mode-icon" @click="toggle_dark_mode()" />
+    
+    <header>
+      <h1>&#9639; &nbsp; Ben H. &nbsp; &#9640;</h1>
+      <p id="page-subtitle">Hi! This is my personal website.</p>
+    </header>
 
-  <div class="divider-line"></div>
+    <nav id="nav-links">
+      <router-link class="nav-link blue" to="/">Bio</router-link>
+      <router-link class="nav-link blue2" to="/portfolio">Portfolio</router-link>
+      <router-link class="nav-link brown" to="/plans">Plans</router-link>
+      <router-link class="nav-link orange" to="/dash">Dash</router-link>
+    </nav>
 
-  <router-view/>
+    <div class="divider-line"></div>
+
+    <router-view/>
+
+  </div>
 </template>
+
+<script>
+
+export default {
+  
+  data() {
+    return {
+      dark_mode: false
+    }
+  },
+  
+  methods: {
+    toggle_dark_mode() {
+      this.dark_mode = !this.dark_mode;
+      localStorage.darkMode = this.dark_mode;
+    }
+  },
+  
+  mounted() {
+    if (String(localStorage.darkMode) == "false") {
+      this.dark_mode = false;
+    } else {
+      this.dark_mode = true;
+    }
+  }
+
+}
+</script>
+
 
 <style> /*  App-wide styles!  */
 
@@ -58,5 +94,27 @@
   color: #2c3e50;
 }
 
+html, body {
+  margin: 0px;
+}
+
+
+/*  Dark mode stuff.   */
+#dark-mode-icon {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+}
+.dark-mode #dark-mode-icon {
+  filter: invert();
+}
+.dark-mode {
+  background: #0D0D0D;
+  color: white;
+}
+.dark-mode a:not(.nav-link) {
+  color: var(--blue2);
+}
 
 </style>
